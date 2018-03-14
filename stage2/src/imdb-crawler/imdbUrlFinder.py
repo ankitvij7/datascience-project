78bce02bd4785b1f2a1ff8c4d4ea0cc1c5e551e7
Unknown <joel@haynie.com>
2018-03-13 22:12:21 -0500

    Crawls the URL for the 250 top and extrcts the lines.

---
+++ stage2/src/imdb-crawler/imdbUrlFinder.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+from imdbCrawler import extract_info
+
+page = requests.get('https://www.imdb.com/chart/top?ref_=nv_mv_250_6')
+soup = BeautifulSoup(page.text, 'html.parser')
+# print(soup.prettify())
+
+top_movies = soup.find_all("td", {"class": "titleColumn"})
+#print(top_movies)
+
+#urls = []
+for tm in top_movies:
+    urlEndFull = tm.find('a', href=True)['href']
+    #print(urlEndFull)
+    urlEnd = urlEndFull[:urlEndFull.rfind("/")+1]
+    #print(urlEnd)
+    # urls.append()
+    url = "https://www.imdb.com" + urlEnd;
+    print(url)
+    extract_info(url)
+
+#print(urls)
