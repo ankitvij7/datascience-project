97887e08e53d97daf4b3091b909dafe6387e07fd
Ankit Vij <ankit.vij1@gmail.com>
2018-03-18 10:40:54 -0500

    NA-0: updatgit statusing crawler

--- stage2/src/rotten-tomatoes-crawler/rotten_tomatoes_crawler.py
+++ stage2/src/rotten-tomatoes-crawler/rotten_tomatoes_crawler.py
@@ -1,12 +1,16 @@
 import bs4 as bs
 
 
-def extract_info(movie_html):
+def extract_info(movie_html, output):
     movie_soup = bs.BeautifulSoup(movie_html, 'html.parser')
-    movie_info = {}
+    movie_info = dict()
     movie_info["Title: "] = movie_soup.find('h1', class_='title').text.strip(' \t\n\r')
     movie_info["TomatoMeter: "] = movie_soup.find('span', class_='meter-value superPageFontColor').find('span').text
-    movie_info["Audience Score: "] = movie_soup.find('div', class_='meter media').find('span', class_='superPageFontColor').text
+    audience_score = movie_soup.find('div', class_='meter media')
+    movie_info["Audience Score: "] = "NA";
+    if audience_score is not None:
+        audience_score = audience_score.find('span', class_='superPageFontColor')
+        movie_info["Audience Score: "] = audience_score.text if audience_score is not None else "0%"
 
     movie_attributes = movie_soup.find_all('li', class_='meta-row clearfix');
     for movie_attribute in movie_attributes:
@@ -23,8 +27,19 @@ def extract_info(movie_html):
 
         movie_info[info_type] = info_value.strip(' \t\n\r')
 
-    complete_movie_info = ""
-    for key, value in movie_info.items():
-        complete_movie_info = complete_movie_info + key + value + ", "
+    # complete_movie_info = ""
+    # for key, value in movie_info.items():
+    #     complete_movie_info = complete_movie_info + key + value + ", "
 
-    print("RESULT IS HERE- " + complete_movie_info)
+    output.writerow({'Title': 'NA' if "Title: " not in movie_info else movie_info["Title: "],
+                     'TomatoMeter': 'NA' if "TomatoMeter: " not in movie_info else movie_info["TomatoMeter: "],
+                     'Audience Score': 'NA' if "Audience Score: " not in movie_info else movie_info["Audience Score: "],
+                     'Rating': 'NA' if "Rating: " not in movie_info else movie_info["Rating: "],
+                     'Genre': 'NA' if "Genre: " not in movie_info else movie_info["Genre: "],
+                     'Directed By': 'NA' if "Directed By: " not in movie_info else movie_info["Directed By: "],
+                     'Written By': 'NA' if "Written By: " not in movie_info else movie_info["Written By: "],
+                     'On Disc/Streaming': movie_info["On Disc/Streaming: "],
+                     'Box Office': 'NA' if "Box Office: " not in movie_info else movie_info["Box Office: "],
+                     'Runtime': 'NA' if "Runtime: " not in movie_info else movie_info["Runtime: "],
+                     'Studio': 'NA' if "Studio: " not in movie_info else movie_info["Studio: "]})
+    # print("RESULT IS HERE- " + complete_movie_info)
