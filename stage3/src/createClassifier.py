a5eac44d248588c034f2cc814c4e6ef1480480a2
Ankit Vij <ankit.vij1@gmail.com>
2018-04-17 23:02:44 -0500

    NA-0: changes to classifier

--- stage3/src/createClassifier.py
+++ stage3/src/createClassifier.py
@@ -2,17 +2,20 @@ import py_entitymatching as em
 import os
 
 # read A and B
-path_A = em.get_install_path() + os.sep + 'datasets' + os.sep + 'end-to-end' + os.sep + 'dblp_demo.csv'
-path_B = em.get_install_path() + os.sep + 'datasets' + os.sep + 'end-to-end' + os.sep + 'acm_demo.csv'
-A = em.read_csv_metadata(path_A, key='id')
-B = em.read_csv_metadata(path_B, key='id')
+path_A = '../data/A.csv'
+path_B = '../data/B.csv'
+A = em.read_csv_metadata(path_A, key='ID')
+B = em.read_csv_metadata(path_B, key='ID')
 # read G
-path_G = em.get_install_path() + os.sep + 'datasets' + os.sep + 'end-to-end' + os.sep + 'labeled_data_demo.csv'
+path_G = '../data/G.csv'
 G = em.read_csv_metadata(path_G,
                          key='_id',
                          ltable=A, rtable=B,
-                         fk_ltable='ltable_id', fk_rtable='rtable_id')
-len(G)
+                         fk_ltable='ltable_ID', fk_rtable='rtable_ID')
+print('Number of tuples in A: ' + str(len(A)))
+print('Number of tuples in B: ' + str(len(B)))
+print('Number of tuples in G: ' + str(len(G)))
+
 
 # create I and J sets
 IJ = em.split_train_test(G, train_proportion=0.7, random_state=0)
@@ -33,17 +36,18 @@ H = em.extract_feature_vecs(I,
                             feature_table=feature_table,
                             attrs_after='label',
                             show_progress=False)
+
 # select best matcher
 # precision
 result = em.select_matcher([dt, rf, svm, ln, lg], table=H,
-                           exclude_attrs=['_id', 'ltable_id', 'rtable_id', 'label'],
+                           exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'label'],
                            k=5,
                            target_attr='label', metric_to_select_matcher='precision', random_state=0)
 print(result['cv_stats'])
 
 # recall
 result = em.select_matcher([dt, rf, svm, ln, lg], table=H,
-                           exclude_attrs=['_id', 'ltable_id', 'rtable_id', 'label'],
+                           exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'label'],
                            k=5,
                            target_attr='label', metric_to_select_matcher='recall', random_state=0)
 print(result['cv_stats'])
@@ -53,11 +57,14 @@ L = em.extract_feature_vecs(J, feature_table=feature_table,
 
 # select the best classifier using the above precision and recall results - for now choosing random forest
 rf.fit(table=H,
-       exclude_attrs=['_id', 'ltable_id', 'rtable_id', 'label'],
+       exclude_attrs=['_id', 'ltable_v', 'rtable_ID', 'label'],
        target_attr='label')
 
-predictions = rf.predict(table=L, exclude_attrs=['_id', 'ltable_id', 'rtable_id', 'label'],
+predictions = rf.predict(table=L, exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'label'],
                          append=True, target_attr='predicted', inplace=False)
 
 eval_result = em.eval_matches(predictions, 'label', 'predicted')
 em.print_eval_summary(eval_result)
+
+I.to_csv(os.fdopen(os.open("I.csv", os.O_RDWR | os.O_CREAT), 'w+'), index=False)
+J.to_csv(os.fdopen(os.open("J.csv", os.O_RDWR | os.O_CREAT), 'w+'), index=False)
